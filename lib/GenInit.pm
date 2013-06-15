b60847c8e7eb43f0a039a8b203e581523e145a46
Miguel Fonseca <miguel.cnf@gmail.com>
2013-06-15 17:50:24 +0100

    Fixing identation

--- lib/GenInit.pm
+++ lib/GenInit.pm
@@ -13,18 +13,14 @@ our @ISA = qw(Exporter);
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
-# This allows declaration	use GenInit ':all';
+# This allows declaration   use GenInit ':all';
 # If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
 # will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
-our @EXPORT = qw(
-	
-);
+our @EXPORT = qw();
 
 our $VERSION = '1.0.0';
 
